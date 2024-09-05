bf00ebc263efacb3109e7f65b659bef34dffeddf
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-09-05 08:05:58 -0300

    chore: Initial setup for backend project

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// app.use(express.json());
+
+// // Conectando ao MongoDB
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(err => console.error(err));
+
+// // Importando as rotas
+// const patientRoutes = require('./routes/patientRoutes');
+// const appointmentRoutes = require('./routes/appointmentRoutes');
+// const medicalRecordRoutes = require('./routes/medicalRecordRoutes');
+// const resourceRoutes = require('./routes/resourceRoutes');
+
+// // Usando as rotas
+// app.use('/api/patients', patientRoutes);
+// app.use('/api/appointments', appointmentRoutes);
+// app.use('/api/medical-records', medicalRecordRoutes);
+// app.use('/api/resources', resourceRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+},);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
